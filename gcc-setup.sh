26cfe6e2ba25ce7945ebfea0d677cc864d6091b7
chenshi <shichen@qiyi.com>
2016-10-31 20:42:21 +0800

    fix

--- gcc-setup.sh
+++ gcc-setup.sh
@@ -18,11 +18,11 @@ make && make install
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 
 cd ..
-echo 'installng gcc-4.9.2...'
-tar -xjf gcc-4.9.2.tar.bz2
-cd gcc-4.9.2
-mkdir gcc-build-4.9.2
-cd gcc-build-4.9.2
+echo 'installng gcc-4.9.3...'
+tar -xjf gcc-4.9.3.tar.bz2
+cd gcc-4.9.3
+mkdir gcc-build-4.9.3
+cd gcc-build-4.9.3
 ../configure --enable-checking=release --enable-language=c,c++,go --disable-multilib --enable-thread=posix
 make -j2 && make install
 
